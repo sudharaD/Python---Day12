e8951f5d2da1e6831925081ce67e8526f7c39aca
sudharaD <sudharad7@gmail.com>
2023-01-11 22:27:29 +0530

    debugging challenge 1

--- debugging_exercises.py
+++ debugging_exercises.py
@@ -4,3 +4,13 @@ if number % 2 == 0:
   print("This is an even number.")
 else:
   print("This is an odd number.")
+
+  for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print([number])
